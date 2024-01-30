aa252e9a64dc068fa393bfec9225884728ed90e6
Michael Fay <52039648+emzmwf@users.noreply.github.com>
2024-01-30 17:10:28 +0000

    Update IntensityChecker.s
    Use varying step to get suitable data in a suitable time. 
    Shift to end loop now added
    Loop will also terminate if intensity not changing

--- IntensityChecker.s
+++ IntensityChecker.s
@@ -8,6 +8,8 @@ Start with vacuum, intensity that is high but not saturating
 //MWF 22 May 2023 initial script
 Script tested on Gatan OneView on JEOL 2100Plus
 Gatan K3-IS on JEOL 2100F
+
+Feb2024 - replace linear step with varying, todo - test params on differing TEMs
 */
 
 //Camera variables
@@ -29,11 +31,26 @@ help += "Hold SHIFT to abort."
 
 if ( !OKCancelDialog( help ) )
  exit( 0 )
- 
- 
+
+// Put a friendly marker in the results window
+Result( "\n ========================== \n" )
+Result( "     Intensity Checker     " )
+Result( "\n ========================== \n" )
+
+//Get the microscope parameters
+number NMag = EMGetMagnification()
+number Focus = EMGetFocus()
+number SptS = EMGetSpotSize()+1
+Result( "\n" )
+Result( "Microscope parameters \n" )
+Result( "Magnification : " + NMag + "\n" )
+Result( "Objective Focus (raw units) : " + Focus + "\n" )
+Result( "Spot size (JEOL display): " + SptS + "\n" )
+
+  
 //Get the camera parameters
 CameraGetDefaultParameters( camID, exposure, xBin, yBin, processing, areaT, areaL, areaB, areaR )
-Result( "\n\n" )
+Result( "\n" )
 Result( "Default parameters for '" + CameraGetName(camID) + "' in the 'Record' setting: \n" )
 Result( "Exposure time (sec) : " + exposure + "\n" )
 Result( "CCD Read-out area   : [" + areaT + "/" + areaL + "/" + areaB + "/" + areaR + "] \n" )
@@ -68,15 +85,19 @@ number BVal = EMGetBrightness()
 string tstring = ""
 number s = 65535	//default value, fully spread
 
-number BrightChange = 2000	//default value, 
-GetNumber("Amount of raw units to change per step", BrightChange, BrightChange )
+number BrightChange = 500	//Suggested values - K3: 500, 
+GetNumber("Amount of raw units to change first step", BrightChange, BrightChange )
 
 
 /////////////////////////
-// for loop to run acquisition - currently equal steps, but brightness changes more significant at higher intensities
-// - todo, test varying step for each iteration, e.g. 1.1 of previous step
+// while loop to run acquisition - varying step for each iteration, e.g. 1.1 of previous step
 /////////////////////////
-for (s = BVal ; s<=65535 ; s = s+BrightChange)
+
+number step = BrightChange
+number oldint = 99999999
+
+s = BVal
+while (s <=65535)
 {
 	EMSetBrightness(s)
 	//tstring = "Brightness "+s+" "
@@ -90,16 +111,26 @@ for (s = BVal ; s<=65535 ; s = s+BrightChange)
  	//Get mean from image
 	number immean = mean(IMG)
 	Result("\n"+imname+": "+immean)	
- 	if (immean<=2)
-  		break//End loop early if counts are too low
-	
-}
+	s = s+step
+	step = round(step*1.1)
+	if (immean <2)
+		break
+	if (immean > oldint){
+		Result("\n intensity not changing, ending loop")
+		break
+		}
+	if  (ShiftDown() == 1){
+		Result("\n Shift pressed, ending loop")
+		break
+		}//end if shift down		
+	oldint = immean
+} 
 
 // Blank the beam
-EMSetBeamBlanked(ON)
+EMSetBeamBlanked(1)
 
 string end = "Brightness Series:\n"
-end += "Acquisiton complete.\n"
+end += "Acquisition complete.\n"
 end += "Beam has been blanked."
 
 //To do - plot brightness vs intensity for images

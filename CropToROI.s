f847a98758538379a68176f4cf4e29357fe999ad
Michael Fay <52039648+emzmwf@users.noreply.github.com>
2024-12-18 16:55:12 +0000

    Update CropToROI.s
    Copy tags, and add additional tag in acquisition with name of source file

--- CropToROI.s
+++ CropToROI.s
@@ -3,9 +3,8 @@
 
 // D. R. G. Mitchell, adminnospam@dmscripting.com (remove the nospam to make this email address work)
 
-// version:20090531
-// v1.2
-
+// v1.3 20241218 - copy tags also, add tag saying name of original file
+// TO DO - scale bar is fine when this is run as a separate script, but size is massive when run from menu. Investigate and fix.
 
 // variables
 
@@ -13,6 +12,31 @@ number sizex, sizey, scalex, scaley
 number top, left, bottom, right
 number roipixwidth, roiunitwidth, roipixheight, roiunitheight
 string unitstring, imgname
+
+
+//copytags
+void copytags(image imgsource, image imgtarget){
+//try
+//gettwoimageswithprompt("0 = Source Image, 1 = Target Image","Copy ALL tags between images", imgsource, imgtarget)
+// 
+//catch
+//exit(0)
+ 
+string targetname, sourcename
+targetname=getname(imgtarget)
+sourcename=getname(imgsource)
+ 
+TagGroup sourcetags=imagegettaggroup(imgsource)
+TagGroup targettags=imagegettaggroup(imgtarget)
+taggroupcopytagsfrom(targettags,sourcetags)
+
+//Add tag to say sourcename
+String tagPath = "Acquisition:Original"
+TagGroupSetTagAsString(targettags, tagPath, sourcename) 
+
+okdialog("All tags copied from '"+sourcename+"' to '"+targetname+"'.")
+}
+
 
 
 // Get info from foremost image
@@ -67,3 +91,6 @@ number substringlength=len(imgname)
 if(substringlength>9) substringlength=9
 string shortname=left(imgname, substringlength)+" Extracted ROI"
 setname(cropped, shortname)
+
+//Copy tags
+copytags(front, cropped)
